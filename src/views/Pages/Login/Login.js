704c5c36550ce9c4073d1ca232b8640585244487
stormsoftwaredevelopment <stormsoftwaredevelopment@gmail.com>
2019-03-18 23:05:47 -0300

    first commit

---
+++ src/views/Pages/Login/Login.js
@@ -0,0 +1,67 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import { Button, Card, CardBody, CardGroup, Col, Container, Form, Input, InputGroup, InputGroupAddon, InputGroupText, Row } from 'reactstrap';
+
+class Login extends Component {
+  render() {
+    return (
+      <div className="app flex-row align-items-center">
+        <Container>
+          <Row className="justify-content-center">
+            <Col md="8">
+              <CardGroup>
+                <Card className="p-4">
+                  <CardBody>
+                    <Form>
+                      <h1>Login</h1>
+                      <p className="text-muted">Faça Login em sua conta</p>
+                      <InputGroup className="mb-3">
+                        <InputGroupAddon addonType="prepend">
+                          <InputGroupText>
+                            <i className="icon-user"></i>
+                          </InputGroupText>
+                        </InputGroupAddon>
+                        <Input type="text" placeholder="Usuário" autoComplete="username" />
+                      </InputGroup>
+                      <InputGroup className="mb-4">
+                        <InputGroupAddon addonType="prepend">
+                          <InputGroupText>
+                            <i className="icon-lock"></i>
+                          </InputGroupText>
+                        </InputGroupAddon>
+                        <Input type="password" placeholder="Senha" autoComplete="current-password" />
+                      </InputGroup>
+                      <Row>
+                        <Col xs="6">
+                        <Link to="/dashboard">
+                          <Button color="primary" className="px-4" style={{backgroundColor:'#51008b',borderWidth:'0em'}}>Entrar</Button>
+                        </Link>
+                        </Col>
+                        <Col xs="6" className="text-right">
+                          <Button color="link" className="px-0">Esqueceu a senha?</Button>
+                        </Col>
+                      </Row>
+                    </Form>
+                  </CardBody>
+                </Card>
+                <Card className="text-black py-5 d-md-down-none" style={{ width: '44%' }}>
+                  <CardBody className="text-center">
+                    <div>
+                      <h2>Registrar</h2>
+                      <p>Ainda não tem uma conta? Registre-se Agora!</p>
+                      <Link to="/register">
+                        <Button color="primary" className="mt-3" active tabIndex={-1} style={{backgroundColor:'#51008b',borderWidth:'0em'}}>Registrar Agora!</Button>
+                      </Link>
+                    </div>
+                  </CardBody>
+                </Card>
+              </CardGroup>
+            </Col>
+          </Row>
+        </Container>
+      </div>
+    );
+  }
+}
+
+export default Login;
